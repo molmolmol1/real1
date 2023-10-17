ee7196ad153f3ccbf929d686071240679b68eb05
User <User@DESKTOP-MJCPUPU>
2023-10-17 15:12:26 +0800

    k

---
+++ lab/src/lab/exer.java
@@ -0,0 +1,14 @@
+package lab;
+
+public class exer {
+
+	public exer() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
