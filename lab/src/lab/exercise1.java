8367e71d13410201b421dac7b5b62628d99aab0f
User <User@DESKTOP-MJCPUPU>
2023-10-17 15:16:15 +0800

    hm

--- lab/src/lab/exercise1.java
+++ lab/src/lab/exercise1.java
@@ -1,5 +1,21 @@
 package lab;
 
-public class exercise1 {
+import java.util.Scanner;
 
-}
+public class exercise1 {
+	//Main method
+		public static void main (String[]args){
+			//Declare variables
+			double pounds;
+			double kilograms;
+			try (//Create a scanner object
+			Scanner input = new Scanner(System.in)) {
+				//Receive inputs from keyboards
+				System.out.println("enter a number in pounds");
+				pounds = input.nextDouble();
+			}
+			//compute kilograms
+			//one pounds is 0.454 kilograms
+			kilograms = pounds * 0.454;
+			//display result
+			System.out.println(pounds + "is" + kilograms + "kilograms");}}
